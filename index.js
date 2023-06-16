4339d3dccbfb1dc17cfdc17f1785b4a5466df115
emma wambui <emma.wambui@student.moringaschool.com>
2023-06-16 08:38:47 +0300

    partial complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+ const name = ["Guadalupe", "Ollie", "Aki"]
+
+ function writeCards(name) {
+  for (let i=0; i<name.length; i++) {
+    return(`Thank you, ${[i]}, for the wonderful surprise gift!`);
+  }
+ }
+ 
+ function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
